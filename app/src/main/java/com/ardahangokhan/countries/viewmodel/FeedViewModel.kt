ff65f54b08d69f31eb0e890fc3acaaadec083851
Ardahan Gökhan <ardahan.gokhan1@gmail.com>
2024-04-04 18:24:25 +0300

    feedviewmodel created

---
+++ app/src/main/java/com/ardahangokhan/countries/viewmodel/FeedViewModel.kt
@@ -0,0 +1,11 @@
+package com.ardahangokhan.countries.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.ardahangokhan.countries.model.Country
+
+class FeedViewModel:ViewModel() {
+    val countries = MutableLiveData<List<Country>>()
+    val countryError = MutableLiveData<Boolean>()
+    val countryLoading = MutableLiveData<Boolean>()
+}
