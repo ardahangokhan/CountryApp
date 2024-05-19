132b58ea83b0b444c3fe7b57daaf39002f39f8d6
Ardahan Gökhan <ardahan.gokhan1@gmail.com>
2024-05-19 23:25:58 +0300

    coroutine added, BaseViewModel created, FeedViewModel revision

--- app/src/main/java/com/ardahangokhan/countries/viewmodel/FeedViewModel.kt
+++ app/src/main/java/com/ardahangokhan/countries/viewmodel/FeedViewModel.kt
@@ -1,15 +1,17 @@
 package com.ardahangokhan.countries.viewmodel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.ardahangokhan.countries.model.Country
 import com.ardahangokhan.countries.service.CountryAPIService
+import com.ardahangokhan.countries.service.CountryDatabase
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class FeedViewModel:ViewModel() {
+class FeedViewModel(application: Application) :BaseViewModel(application){
     private val countryApiService = CountryAPIService()
     private val disposable = CompositeDisposable()
 
@@ -32,11 +34,7 @@ class FeedViewModel:ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<List<Country>>(){
                     override fun onSuccess(t: List<Country>) {
-
-                        countries.value = t
-                        countryError.value = false
-                        countryLoading.value = false
-
+                    storeInSQLite(t)
                     }
 
                     override fun onError(e: Throwable) {
@@ -47,5 +45,29 @@ class FeedViewModel:ViewModel() {
                     }
                 })
         )
+    }
+    private fun showCountries(countryList: List<Country>){
+
+        countries.value = countryList
+        countryError.value = false
+        countryLoading.value = false
+    }
+    private fun storeInSQLite(list:List<Country>){
+        //normalde launch diyerek doğrudan coroutines i kullanabiliyoruz ancak bu fonksiyon bunu bilmediğim için öncelikle bunu tanıtmamız gerekiyor.
+
+        launch {
+            val dao = CountryDatabase(getApplication()).countryDao()
+            dao.deleteAllCountries()
+            //suspent fonksyonun içerisinde ne varsa önce sildik sonra;
+            val listLong = dao.insertAll(*list.toTypedArray())
+        //bir listeyi tekil eleman haline getirmek kotlin in bir özelliği ve biz burada bunu kullanıyoruz. -->individual
+            var i = 0
+            while (i<list.size){
+                list[i].uuid = listLong[i].toInt()
+                i += 1
+                //uuid opsiyonel olduğu için burada tanımlanan bir öğeye uuid verme işlemi sağladık ve sonrasında showcountries ile bunları gösterdik.
+            }
+            showCountries(list)
+        }
     }
 }
